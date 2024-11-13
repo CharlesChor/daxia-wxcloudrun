28ebc3a918107ac33651ab34ecf69bbcb510354a
charleschor <28964245@qq.com>
2024-11-13 17:56:48 +0800

    add wx.login api

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,10 +1,14 @@
 from datetime import datetime
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+import os,requests
 
+# 从环境变量中获取微信小程序的 AppID 和 AppSecret
+APPID = os.getenv('WECHAT_APPID')
+APPSECRET = os.getenv('WECHAT_APPSECRET')
 
 @app.route('/')
 def index():
@@ -12,6 +16,43 @@ def index():
     :return: 返回index页面
     """
     return render_template('index.html')
+
+# 微信小程序登录的路由
+@app.route('/wechat/login', methods=['POST'])
+def wechat_login():
+    # 从请求中获取 code
+    data = request.json
+    code = data.get('code')
+    
+    if not code:
+        return jsonify({'error': 'missing code'}), 400
+
+    # 向微信服务器请求 session_key 和 openid
+    url = f'https://api.weixin.qq.com/sns/jscode2session?appid={APPID}&secret={APPSECRET}&js_code={code}&grant_type=authorization_code'
+    response = requests.get(url)
+    
+    if response.status_code != 200:
+        return jsonify({'error': 'failed to get session'}), 500
+    
+    # 解析响应数据
+    result = response.json()
+    openid = result.get('openid')
+    session_key = result.get('session_key')
+    
+    if not openid:
+        return jsonify({'error': 'failed to get openid'}), 500
+    
+    # TODO: 将 openid 和 session_key 保存到数据库，并创建会话或令牌
+    # 例如，你可以创建一个 JWT 令牌或使用其他会话管理机制
+    
+    # 返回成功响应，实际应用中可能需要返回一个令牌
+    return jsonify({'message': 'login successful', 'openid': openid})
+
+# 健康检查路由
+@app.route('/healthz')
+def health_check():
+    return 'OK'
+
 
 
 @app.route('/api/count', methods=['POST'])
